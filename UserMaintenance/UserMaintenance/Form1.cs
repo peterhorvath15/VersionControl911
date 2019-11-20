e1bf42abfc662f5a842be570ce62cab99993626f
Péter Horváth <peter.horvath15@stud.uni-corvinus.hu>
2019-11-20 18:51:59 +0100

    taskcommit

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -17,8 +17,7 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            lblLastName.Text = Resource1.LastName; // label1
-            lblFirstName.Text = Resource1.FirstName; // label2
+            lblFullName.Text = Resource1.FullName; // label1
             btnAdd.Text = Resource1.Add; // button1
 
             // listbox1
@@ -28,8 +27,7 @@ namespace UserMaintenance
 
             var u = new User()
             {
-                LastName = txtLastName.Text,
-                FirstName = txtFirstName.Text
+                FullName = txtFullName.Text,
             };
             users.Add(u);
         }
